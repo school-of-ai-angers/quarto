58d30999816f453b96fbaae94f724420437541ce
Alexandre Blouin <alexandre.blouin@maplink.global>
2019-10-17 13:18:06 +0200

    Implement Quarto Environment

--- quarto/environment.py
+++ quarto/environment.py
@@ -14,26 +14,66 @@ class Environment:
         self.board_state = np.full((17,), -1)
         self.action_space = []
         self.available_pieces = []
-        self.available_positions = []
+        self.available_positions = [] 
+        rows = [np.arange(4) + 4*l for l in range(4)]
+        columns = [np.arange(16, step=4) + l for l in range(4)]
+        diagonals = [np.array([0, 5, 10, 15]), np.array([3, 6, 9, 12])]
+        self.lines = rows + columns + diagonals
 
     def reset(self):
         self.board_state = np.full((17,), -1)
-        self.action_space = [(0, piece) for piece in range(16)]
-        self.available_pieces = list(range(16))
+        self.action_space = [(pos, piece) for pos in range(16) for piece in range(15)]
+        self.available_pieces = list(range(15))
         self.available_positions = list(range(16))
+        self.board_state[16] = 15
         return self.board_state, self.action_space
 
-    def has_common_trait(self, p1, p2, p3, p4):
+    def has_common_trait(self, pieces):
+        assert len(pieces) == 4
+        p1, p2, p3, p4 = pieces
         return p1 != -1 and p2 != -1 and p3 != -1 and p4 != -1 and ((p1 & p2 & p3 & p4) or ((~p1 & ~p2 & ~p3 & ~p4) & 0xF))
 
+    def get_board_status(self):
+        """
+        check board status.
+        Return 2 is the board as a winning line
+        Return 1 in case of draw
+        Return 0 otherwise
+        """
+        for line in self.lines:
+            if self.has_common_trait(self.board_state[line]):
+                return 2
+        return 1 if len(self.available_positions)==0 else 0
+
+
     def step(self, action):
-        # Todo
         assert action in self.action_space, "Invalid action was taken. Please check call to step"
+
+        # Put the piece on the board
         self.board_state[action[0]] = self.board_state[16]
+        self.available_positions.remove(action[0])
+
+        # Select the next piece for the opponent
         self.board_state[16] = action[1]
         self.available_pieces.remove(action[1])
-        self.available_positions.remove(action[0])
-        reward = 0
-        done = False
-        action_space = [(pos, piece) for pos in self.available_positions for piece in self.available_pieces]
-        return self.board_state[16], reward, done, action_space
+        
+        # Check the environment status
+        status = self.get_board_status()
+        done = status > 0
+        if done:
+            reward = 100 if status == 2 else 0
+            return self.board_state, reward, done, []
+
+        # Finalize the game if the last piece where chosen
+        if len(self.available_pieces)==0:
+            last_position = self.available_positions[0]
+            self.board_state[last_position] = self.board_state[16]
+            self.available_positions.remove(last_position)
+            status = self.get_board_status()
+            assert status > 0
+            reward = -100 if status == 2 else 0
+            return self.board_state, reward, True, []
+
+        # update list of possible actions
+        self.action_space = [(pos, piece) for pos in self.available_positions for piece in self.available_pieces]
+        return self.board_state, 0, done, self.action_space
--- quarto/tests/test_environment.py
+++ quarto/tests/test_environment.py
@@ -4,4 +4,53 @@ from quarto.environment import Environment
 def test_has_common_trait():
     env = Environment()
 
-    assert env.has_common_trait(-1, -1, -1, -1)
+    assert not env.has_common_trait([-1, -1, -1, -1])
+    assert not env.has_common_trait([-1, 1, 3, 5])
+    assert env.has_common_trait([9, 1, 3, 5])
+    assert env.has_common_trait([2, 4, 0, 8])
+    assert not env.has_common_trait([2, 4, 1, 8])
+    assert env.has_common_trait([0, 4, 8, 12])
+
+
+def test_board_status():
+    env = Environment()
+    env.reset()
+    assert not env.get_board_status()
+
+    env.board_state[0] = 0
+    env.board_state[1] = 4
+    env.board_state[2] = 8
+    env.board_state[3] = 12
+    assert env.get_board_status() == 2
+
+    env.reset()
+    env.board_state[0] = 0
+    env.board_state[5] = 4
+    env.board_state[1] = 1
+    env.board_state[2] = 2
+    env.board_state[3] = 15
+    assert not env.get_board_status()
+    env.board_state[10] = 8
+    env.board_state[15] = 12
+    assert env.get_board_status() == 2
+
+    import numpy as np
+
+    # TODO : construct a draw game
+    env.reset()
+    env.step((15, 10))
+    env.step((0, 5))
+    env.step((1, 7))
+    env.step((2, 8))
+    env.step((3, 9))
+    env.step((4, 11))
+    env.step((5, 1))
+    env.step((6, 4))
+    env.step((7, 3))
+    env.step((8, 12))
+    env.step((9, 0))
+    env.step((10, 14))
+    env.step((11, 6))
+    env.step((12, 2))
+    env.step((13, 13))
+    assert env.get_board_status() == 1
