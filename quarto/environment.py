18e73c2588ed3b976bb7ef871ff9347c73f9d484
Guilherme <sitegui@sitegui.com.br>
2019-10-28 23:44:52 +0100

    Add support to display() for Environment

--- quarto/environment.py
+++ quarto/environment.py
@@ -4,6 +4,9 @@ rows = [np.arange(4) + 4*l for l in range(4)]
 columns = [np.arange(16, step=4) + l for l in range(4)]
 diagonals = [np.array([0, 5, 10, 15]), np.array([3, 6, 9, 12])]
 
+with open('quarto/static/style.css') as fp:
+    css = fp.read()
+
 
 class Environment:
 
@@ -31,7 +34,8 @@ class Environment:
 
     def reset(self):
         self.board_state = np.full((17,), -1)
-        self.action_space = [16 * pos + piece for pos in range(16) for piece in range(15)]
+        self.action_space = [16 * pos +
+                             piece for pos in range(16) for piece in range(15)]
         self.available_pieces = list(range(15))
         self.available_positions = list(range(16))
         self.board_state[16] = 15
@@ -86,59 +90,50 @@ class Environment:
             return self.board_state, reward, True, []
 
         # update list of possible actions
-        self.action_space = [16 * pos + piece for pos in self.available_positions for piece in self.available_pieces]
+        self.action_space = [
+            16 * pos + piece for pos in self.available_positions for piece in self.available_pieces]
         return self.board_state, 0, done, self.action_space
 
-    def to_jsonable(self):
-        """
-        Return a representation of the current state that can be encoded as JSON.
-        This will be used later to visually display the game state at each step
-        """
-        return self.board_state.tolist()
+    def ask_action(self):
+        position = input('Choose a position: ')
+        piece = input('Choose a piece: ')
+        return 16 * int(position) + int(piece)
 
-    @staticmethod
-    def html_head():
+    def _repr_html_(self):
         """
-        Return HTML string to inject at the page's <head>
-        """
-        return '<link rel="stylesheet" href="/static/environment-quarto/style.css">'
-
-    @staticmethod
-    def jsonable_to_html(jsonable):
-        """
-        Return a HTML representation of the game at the given state
+        Display a HTML representation of the game at this state in a Python Notebook
         """
 
         # Detect winning line
         win_line = []
-        state = np.asarray(jsonable)
         for line in Environment.lines:
-            if Environment.has_common_trait(state[line]):
+            if Environment.has_common_trait(self.board_state[line]):
                 win_line = line
                 break
 
         # Build main board divs
         main_board = '\n'.join(
-            f'<div class="quarto-position-{pos} {"quarto-piece-" + str(piece) if piece != -1 else ""} {"quarto-win-line" if pos in win_line else ""}">{pos}</div>'
-            for pos, piece in enumerate(jsonable)
+            f'<div class="quarto-position-{pos} {"quarto-piece-" + str(int(piece)) if piece != -1 else ""} {"quarto-win-line" if pos in win_line else ""}">{pos}</div>'
+            for pos, piece in enumerate(self.board_state)
         )
 
         # Build reserve divs
         reserve_1 = '\n'.join(
-            f'<div class="quarto-piece-{i} col {"quarto-reserve-used" if i in jsonable else ""}">{i}</div>'
+            f'<div class="quarto-piece-{i} quarto-reserve-col {"quarto-reserve-used" if i in self.board_state else ""}">{i}</div>'
             for i in range(0, 8)
         )
         reserve_2 = '\n'.join(
-            f'<div class="quarto-piece-{i} col {"quarto-reserve-used" if i in jsonable else ""}">{i}</div>'
+            f'<div class="quarto-piece-{i} quarto-reserve-col {"quarto-reserve-used" if i in self.board_state else ""}">{i}</div>'
             for i in range(8, 16)
         )
 
         return f'''
+        <style>{css}</style>
         <div class="quarto-board {"quarto-win" if len(win_line) else ""}">
             {main_board}
         </div>
-        <div class="quarto-reserve container">
-            <div class="row">{reserve_1}</div>
-            <div class="row my-2">{reserve_2}</div>
+        <div class="quarto-reserve">
+            <div class="quarto-reserve-row">{reserve_1}</div>
+            <div class="quarto-reserve-row">{reserve_2}</div>
         </div>
         '''
